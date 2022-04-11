2d5c209a1cb490f15ce3bfcc884c2cb73f54c265
emagdnimMQT <mark.w.novak@gmail.com>
2022-04-11 09:56:45 -0400

    Started working on game script.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-//check out number guesser from codecademy project
+const questionNumber = document.getElementById('question-number');
+const nextQuestionButton = document.getElementById('next-question');
+
+let currentQuestionNumber = 1;
+
+const advanceRound = () => currentQuestionNumber++;
+
+nextQuestionButton.addEventListener('click', () => {
+    //increase question number
+    if (questionNumber.innerText < 3) {
+        advanceRound();
+    }
+    //display the new question number
+    questionNumber.innerText = currentQuestionNumber;
+})
